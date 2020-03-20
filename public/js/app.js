fdf26c21d3d3c33ad31b86f5946995ff47694034
kevinparada99 <geovannyparada99@gmail.com>
2020-03-19 21:22:42 -0600

    mini

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,39 @@
 var $ = el => document.querySelector(el);
 document.addEventListener("DOMContentLoaded",event=>{
-    let mostrarVista = $("[class*='mostrar']");
-    mostrarVista.addEventListener('click',e=>{
-        e.stopPropagation();
+    let alumnos = Document.getElementById("idalumno");
+    let docentes = Document.getElementById("iddocentes");
 
-        let modulo = e.srcElement.dataset.modulo;
-        
-        fetch('public/vistas/alumnos/alumnos.html').then( resp=>resp.text() ).then(resp=>{
-            $(`#vistas-${modulo}`).innerHTML = resp;
-            
-            let btnCerrar = $(".close");
-            btnCerrar.addEventListener("click",event=>{
-                $(`#vistas-${modulo}`).innerHTML = "";
-            });
-
-            let cuerpo = $("body"),
-                script = document.createElement("script");
-            script.src = `public/vistas/${modulo}/${modulo}.js`;
-            cuerpo.appendChild(script);
-        });
-
+  docentes.addEventListener("click", e=>{
+     e.stopPropagation();
+      let vistas = "docentes";
+     colocarVista(vistas);
     });
 
-
+    alumnos.addEventListener("click", e=>{
+        e.stopPropagation();
+        let vistas = "alumnos";
+        colocarVista(vistas);
+    });
 });
+   
+
+
+
+function colocarVista(vistas){
+
+    fetch(`public/vistas/${vistas}/${vistas}.html`).then( resp => resp.text()).then( resp => {
+
+        document.getElementById(`vistas-${vistas}`).innerHTML = resp;
+        let btnCerrar = $(".close");
+
+        btnCerrar.addEventListener("click", event => {
+            $(`#vistas-${vistas}`).innerHTML = "";
+        });
+
+        let cuerpo = $("body"), script = document.createElement("script");
+        script.src = `public/vistas/${vistas}/${vistas}.js`;
+        cuerpo.appendChild(script);
+            
+    });
+
+ }
