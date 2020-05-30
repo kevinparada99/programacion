e14ba1662e8a0425f72a35f14d97ef1a2e457202
kevinparada99 <geovannyparada99@gmail.com>
2020-05-29 20:02:09 -0600

    listoo

--- public/vistas/inicio.php
+++ public/vistas/inicio.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://unpkg.com/vue-select@3.0.0/dist/vue-select.css">
     <link rel="stylesheet" href="../../../public/Nutricion-master/css/estilo.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="../../../public/alertifyjs/css/alertify.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Control</title>
@@ -15,35 +16,22 @@
        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato'>
 </head>
 <body class="bg-secondary">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-       <img src="../../../public/Nutricion-master/img/logo1.png" alt=""> 
-       
-       
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="../cerrar.php">INICIO <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mostrar-usuarios" data-modulo="usuarios" data-form="usuarios" href="#">Usuarios</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link mostrar-productos" data-modulo="productos" data-form="productos" href="#">Productos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mostrar-medicamentos" data-modulo="medicamentos" data-form="medicamentos" href="#">Medicamentos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mostrar-recetas" data-modulo="recetas" data-form="recetas" href="#">Recetas</a>
-                </li> 
-                <li class="nav-item">
-                    <a class="nav-link mostrar-controles" data-modulo="controles" data-form="controles" href="#">Controles</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mostrar-nutriciones" data-modulo="nutriciones" data-form="nutriciones" href="#">Nutriciones</a>
-                </li>
-            </ul>
-        </div>
+    <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+   </label>    
+    <label class="logo"> Nutri5Tech</label>
+        <ul>
+        <li> <a class="active" href="../cerrar.php">INICIO <span class="sr-only">(current)</span></a></li>
+        <li> <a class="nav-link mostrar-usuarios" data-modulo="usuarios" data-form="usuarios" href="#">Usuarios</a></li>
+        <li> <a class="nav-link mostrar-productos" data-modulo="productos" data-form="productos" href="#">Productos</a></li>
+        <li> <a class="nav-link mostrar-medicamentos" data-modulo="medicamentos" data-form="medicamentos" href="#">Medicamentos</a></li>
+        <li> <a class="nav-link mostrar-recetas" data-modulo="recetas" data-form="recetas" href="#">Recetas</a></li>
+        <li> <a class="nav-link mostrar-controles" data-modulo="controles" data-form="controles" href="#">Controles</a></li>
+        <li> <a class="nav-link mostrar-nutriciones" data-modulo="nutriciones" data-form="nutriciones" href="#">Nutriciones</a></li>
+            
+        </ul>
     </nav>
     
 
